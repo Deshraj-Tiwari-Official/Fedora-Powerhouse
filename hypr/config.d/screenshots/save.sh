240b837c1418b47b29f0b1609f529887255b7b6d
Deshraj-Tiwari-Official <deshrajtiwari690@gmail.com>
2025-03-29 08:37:52 -0400

    Fixing missed things

--- hypr/config.d/screenshots/save.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-timestamp=$(date +'%Y%m%d_%H%M%S')
-screenshot_path="$HOME/Pictures/Screenshots/${timestamp}.png"
-grim -g "$(slurp -d)" "$screenshot_path"
-wl-copy < "$screenshot_path"
-dunstify "Screenshot saved! as $screenshot_path" -i "$screenshot_path"
