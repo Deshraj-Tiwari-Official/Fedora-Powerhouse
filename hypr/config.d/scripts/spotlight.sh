6ee377eb8bf7b874f19281ef1d98a3aa8481b63d
Deshraj-Tiwari-Official <deshrajtiwari690@gmail.com>
2025-05-22 18:27:17 +0530

    Spotlight Search Functionality Initialization

---
+++ hypr/config.d/scripts/spotlight.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+main_menu=" Apps\n󰖟 Browser Search\n Calc\n Emoji\n Files\n Power Menu"
+
+choice=$(echo -e "$main_menu" | wofi --dmenu --prompt "Spotlight Search")
+
+case "$choice" in
+  " Apps")
+    wofi --show drun
+    ;;
+  "󰖟 Browser Search")
+    query=$(wofi --dmenu --height 1)
+    if [[ -n "$query" ]]; then
+      xdg-open "https://duckduckgo.com/?q=$(echo "$query" | sed 's/ /+/g')"
+    fi
+    ;;
+  " Calc")
+    expr=$(wofi --dmenu --height 1)
+    result=$(echo "$expr" | bc -l 2>/dev/null)
+    if [[ -n "$result" ]]; then
+      notify-send " $expr = $result"
+    else
+      notify-send " Invalid expression"
+    fi
+    ;;
+  " Emoji")
+    ~/.config/hypr/config.d/scripts/spotlight_feats/emoji_picker.sh
+    ;;
+  " Files")
+    ########### FILE SEARCH LOGIC HERE ###########
+    ;;
+  " Power Menu")
+    ~/.config/hypr/config.d/scripts/spotlight_feats/powermenu.sh
+    ;;
+  *)
+    exit 0
+    ;;
+esac
