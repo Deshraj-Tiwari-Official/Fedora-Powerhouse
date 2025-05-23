6c84e30681507520546ba5038f8e47c849420eaf
Deshraj-Tiwari-Official <deshrajtiwari690@gmail.com>
2025-05-23 22:11:06 +0530

    Harpoon integration neovim

--- nvim/lua/dash/plugins/plugins.lua
+++ nvim/lua/dash/plugins/plugins.lua
@@ -63,4 +63,33 @@ return {
     ---@type render.md.UserConfig
     opts = {},
   },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    lazy = false,
+    config = function()
+      local harpoon = require('harpoon')
+      harpoon:setup({})
+
+      -- File Operations
+      vim.keymap.set('n', '<leader>ha', function() harpoon:list():append() end, { desc = 'Harpoon: Add current file' })
+      vim.keymap.set('n', '<leader>he', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Harpoon: List all Harpoon files' })
+      vim.keymap.set('n', '<leader>hx', function() harpoon:list():clear() end, { desc = 'Harpoon: Clear all Harpoon files' })
+
+      -- File Selection
+      vim.keymap.set('n', '<leader>hh', function() harpoon:list():select(1) end, { desc = 'Harpoon: Select Harpoon file 1'})
+      vim.keymap.set('n', '<leader>hj', function() harpoon:list():select(2) end, { desc = 'Harpoon: Select Harpoon file 2'})
+      vim.keymap.set('n', '<leader>hk', function() harpoon:list():select(3) end, { desc = 'Harpoon: Select Harpoon file 3'})
+      vim.keymap.set('n', '<leader>hl', function() harpoon:list():select(4) end, { desc = 'Harpoon: Select Harpoon file 4'})
+
+    end,
+  },
 }
